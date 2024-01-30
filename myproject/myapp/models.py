fac055e52070a28e1f437ffddd2d26e03424684f
Natdanai <natdanai.thaiyala@gmail.com>
2024-01-30 16:58:27 +0700

    Create model

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+#Person = Table in DB
+class Person(models.Model):
+    #name age date = Field in DB
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    date = models.DateField(auto_now_add=True)
