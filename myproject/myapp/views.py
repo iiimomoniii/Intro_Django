8c416233f04ce9a88f7e716ba854032bc7665dde
Natdanai <natdanai.thaiyala@gmail.com>
2024-01-31 15:05:47 +0700

    display data in Person Model on index

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from myapp.models import Person
 
 def index(request):
     #response only index.html 
     #return render(request,"index.html")
+
     #response index.html and objects
-    name = "Admin"
-    age = 10
-    return render(request,"index.html", {"firstName":name, "age":age})
+    # name = "Admin"
+    # age = 10
+    
+    #response object in models
+    all_person = Person.objects.all()
+    return render(request,"index.html", {"all_person":all_person})
 
 def about(request):
     return render(request,"about.html")
